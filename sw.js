91d0962945292e8c8b84b6fb32758e098edcf58d
itsviejon <dcalflo8@gmail.com>
2021-10-19 11:43:30 -0500

    error sw

--- sw.js
+++ sw.js
@@ -1,46 +1,42 @@
-const CACHE_NAME = 'cache-v1'
+const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
-const CACHE_STATIC_NAME = 'static-v1'
-const CACHE_DYNAMIC_NAME = 'dynamic-v1'
-const CACHE_INMUTABLE_NAME = 'inmutable-v1'
-
-function cleanCache(cacheName, sizeItems){
-    caches.open(cacheName)
-        .then((response) => {
-            response.keys().then(keys => {
-                console.log(keys);
-                if (keys >= sizeItems) {
-                    response.delete(keys[0]).then(()=>{
-                        clearCache(cacheName,sizeItems)
-                    });
-                }
-            });
+function cleanCache(cacheName, sizeItem){
+    caches.open(cacheName).then((cache)=>{
+        cache.keys().then((keys)=>{
+            console.log(keys);
+            if(keys.length >= sizeItem){
+                cache.delete(keys[0]).then(
+                    cleanCache(cacheName, sizeItem)
+                );
+            }
         });
+    });
 }
 
-self.addEventListener('install', (event) => {
-    console.log('SW installed');
+self.addEventListener('install', (event)=>{
+    // Crear caché y almacenar nuestro APPSHELL (tdo lo que ocupa la app para que se pueda ejecutar)
+    const promesaCache = caches.open(CACHE_STATIC_NAME).then((cache)=>{
+        return cache.addAll([
+            '/',
+            'index.html',
+            'css/page.css',
+            'img/inicio.jpg',
+            'js/app.js',
+            'pages/view-offline.html'
+        ]);
+    });
 
-    const promesaChaches = caches.open(CACHE_STATIC_NAME)
-        .then(cache => {
-            return cache.addAll([
-                '/',
-                'index.html',
-                'css/page.css',
-                'img/kitten.png',
-                'js/app.js',
-                'pages/view-offline.html'
-            ]);
-        });
+    const promInmutable = caches.open(CACHE_INMUTABLE_NAME).then((cacheIn)=>{
+        return cacheIn.addAll([
+            'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css'
+        ]);
+    });
 
-        const promInmutable = caches.open(CACHE_INMUTABLE_NAME)
-            .then(cacheInmutable => {
-                return cacheInmutable.addAll([
-                    'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css'
-                ]);
-            });
+    
 
-        event.waitUntil(Promise.all([promesaChaches, promInmutable]));
+    event.waitUntil(Promise.all([promesaCache, promInmutable]));
 });
 
 self.addEventListener('activate', (event)=>{
@@ -56,71 +52,79 @@ self.addEventListener('activate', (event)=>{
     event.waitUntil(resDelCache)
 })
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event)=>{
 
-    // 3. network with cache fallback
+    // 3.- Nerwork with cache fallback
+    /*const respuesta = fetch(event.request).then((res) =>{
+        if(!res){
+            //aquí se retorno la respuesta generica
+            return caches.match(event.request).then((resCache) =>{
+                console.log(resCache);
+                return resCache;
+            }).catch((error) =>{
+                console.log(error);
+            })
+        }
 
-    // const respuesta = fetch(event.request).then(res => {
-        
-    //     if (!res) {
-    //         return caches.match(event.request)
-    //         .then(resCache => {
-    //             return resCache;
-    //         })
-    //         .catch(err => {
-    //             log.error(err);
-    //         });
-    //     }
+        caches.open(CACHE_DYNAMIC_NAME).then((cache) =>{
+            cache.put(event.request, res);
+            cleanCache(CACHE_DYNAMIC_NAME, 5);
+        })
+        return res.clone();
+    }).catch((error) =>{
+        // Excepción en el fetch
+        // Tratar de retornar algo que está en caché
 
-    //     caches.open(CACHE_DYNAMIC_NAME)
-    //         .then(cache => {
-    //             cache.put(event.request, res);
-    //             clearCache(CACHE_DYNAMIC_NAME, 5);
-    //         });
-    //     return res.clone();
-    // })
-    // .catch(err => {
-    //     return caches.match(event.request);
-    // });
+        //aquí se retorno la respuesta generica
+        return caches.match(event.request).then((resCache) =>{
+            console.log(resCache);
+            return resCache;
+        }).catch((error) =>{
+            console.log(error);
+        })
+    });
 
-    // event.respondWith(respuesta);
-    // 2. Cache with network fallback
-    // cache first, then WEB
+    event.respondWith(respuesta);*/
 
-    const respuestaCache = caches.match(event.request)
-        .then(resp => {
-            if(resp){
-                return resp;
+    // 2.- Cache with network fallback
+    // Primero busca caché y si no lo encuentra va a la red
+    const respuestaCache = caches.match(event.request).then((resp)=>{
+        // Si mi request existe en cache
+        if(resp){
+            //respondemos con cache
+            return resp;
+        }
+        console.log('No está en caché', event.request.url);
+        // voy a la red
+        return fetch(event.request).then((respuestaNetwork)=>{
+            // abro cache
+            caches.open(CACHE_DYNAMIC_NAME).then((cache)=>{
+                // guardo la respuesta de la red en cache
+                cache.put(event.request, respuestaNetwork).then(()=>{
+                    cleanCache(CACHE_DYNAMIC_NAME, 4)
+                });
+                
+            });
+            // respondo con el response de la red
+            return respuestaNetwork.clone();
+        }).catch(()=>{
+            console.log('Error al solicitar el recurso');
+
+            if(event.request.headers.get('accept').includes('text/html')){
+                return caches.match('/pages/view-offline.html')
             }
 
-            console.log('Is not on chache', event.request.url);
+            if(event.request.headers.get('accept').includes('image')){
+                return caches.match('/img/default.jpg')
+            }
 
-            return fetch(event.request)
-                .then(respNet => {
-                    caches.open(CACHE_DYNAMIC_NAME)
-                        .then(cache => {
-                            cache.put(event.request, respNet)
-                                .then(cleanCache(CACHE_DYNAMIC_NAME,4 ));
-                        });
-                    return respNet.clone();
-                })
-                .catch(()=>{
-                    console.log('Error al solicitar el recurso');
+        });
+    });
+    event.respondWith(respuestaCache);
 
-                    if(event.request.headers.get('accept').includes('text/html')){
-                        return caches.match('/pages/view-offline.html')
-                    }
 
-                    if(event.request.headers.get('accept').includes('image')){
-                        return caches.match('/img/default.jpg')
-                    }
-                });
-            });
-        event.respondWith(respuestaCache)
-
-    // 1. Only cache
-    // event.respondWith(caches.match(event.request));
-
+    // 1.- Only cache
+    //event.respondWith(caches.match(event.request));
 });
 
 
